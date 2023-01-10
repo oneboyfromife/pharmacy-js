2d4c70f0b428f7259bf0a326822345b83353f68b
kingjamesegunn <james@steer.global>
2023-01-10 14:34:13 +0100

    Fixed import issues

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,10 @@
 import {
-  ImageBackground,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
+	ImageBackground,
+	SafeAreaView,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View,
 } from "react-native";
 import React from "react";
 import TabBar from "../components/TabBar";
@@ -12,38 +12,38 @@ import HomeHeader from "../components/HomeHeader";
 import HomeCard from "../components/HomeCard";
 
 const HomeScreen = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>HomeScreen</Text>
-      <HomeHeader
-        OpeningTime="Opening Time: 10:00 AM"
-        ClosingTime="Closing Time: 2:00 PM"
-      />
-      <View style={{ marginTop: 5 }}>
-        <ImageBackground
-          source={require("C:/Users/Owojori Akintunde/Documents/MOBILE/pharmacy-js/assets/HomeBanner.png")}
-          style={{ width: 356, height: 93, resizeMode: "cover" }}
-        />
-        <TouchableOpacity style={{ backgroundColor: "#000000" }}>
-          <Text style={{ fontSize: 10, color: "white" }}>Open</Text>
-        </TouchableOpacity>
-      </View>
+	return (
+		<SafeAreaView style={styles.container}>
+			<Text>HomeScreen</Text>
+			<HomeHeader
+				OpeningTime="Opening Time: 10:00 AM"
+				ClosingTime="Closing Time: 2:00 PM"
+			/>
+			<View style={{ marginTop: 5 }}>
+				<ImageBackground
+					source={require("../assets/HomeBanner.png")}
+					style={{ width: 356, height: 93, resizeMode: "cover" }}
+				/>
+				<TouchableOpacity style={{ backgroundColor: "#000000" }}>
+					<Text style={{ fontSize: 10, color: "white" }}>Open</Text>
+				</TouchableOpacity>
+			</View>
 
-      <View style={{}}>
-        <HomeCard />
-        <HomeCard />
-      </View>
+			<View style={{}}>
+				<HomeCard />
+				<HomeCard />
+			</View>
 
-      <TabBar />
-    </SafeAreaView>
-  );
+			<TabBar />
+		</SafeAreaView>
+	);
 };
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-  },
+	container: {
+		flex: 1,
+		flexDirection: "column",
+	},
 });
